52f7fa23c366ada8cf926867f1dea3e807815973
ArtyomLinnik <tema.linnik1995@gmail.com>
2015-03-17 02:17:39 +0300

    create Word class file

--- app/src/main/java/com/example/helloworld/Word.java
+++ app/src/main/java/com/example/helloworld/Word.java
@@ -1,5 +1,19 @@
 package com.example.helloworld;
 
+import java.util.List;
+
+
 public class Word {
+    List<EditCell> str;
+
+    public void setStr(List<EditCell> str) {
+        this.str = str;
+
+    }
+
+
+
+
+
 
 }
