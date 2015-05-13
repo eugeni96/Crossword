912ee037d8e6fbc0abb585c7280825fae99dca04
Artyom <tema.linnik1995@gmail.com>
2015-05-14 00:59:16 +0300

    [+] table

--- app/src/main/java/com/example/helloworld/Activity/MainActivity.java
+++ app/src/main/java/com/example/helloworld/Activity/MainActivity.java
@@ -2,18 +2,40 @@ package com.example.helloworld.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.TableLayout;
 
+import com.example.helloworld.DataBase.DBHelper;
 import com.example.helloworld.EditCell;
 import com.example.helloworld.GridCreator;
 import com.example.helloworld.R;
 import com.example.helloworld.Word;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    EditText etWord, etMask;
+
+    DBHelper dbHelper;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,5 +57,42 @@ public class MainActivity extends Activity {
 
         Word firstWord = new Word(cells);
 
+        setContentView(R.layout.activity_main);
+        dbHelper = new DBHelper(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        ContentValues cv = new ContentValues();
+
+        // получаем данные из полей ввода
+        String word = etWord.getText().toString();
+        String mask = etMask.getText().toString();
+
+        // подключаемся к БД
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "Word", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            Log.d(LOG_TAG,  "--- onCreate database ---");
+            //Создаем таблицу с полями
+            db.execSQL("create table Word  ("
+                + "id integer primary key autoincrement,"
+                + "word,"
+                + "question" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+        }
     }
 }
